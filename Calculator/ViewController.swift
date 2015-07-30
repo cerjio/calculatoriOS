5846ea35d2dd9b5386fa1add6298706a3ab20e07
cerjio <cerjio@gmail.com>
2015-07-30 12:39:26 -0500

    Uploading constraints and the core calculation

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
   
     @IBOutlet weak var display: UILabel!
     
-    var userIsInTheMiddleOfTypingANumber: Bool = false
+    var userIsInTheMiddleOfTypingANumber = false
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -25,6 +25,69 @@ class ViewController: UIViewController {
         } else {
             display.text = digit
             userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        switch operation {
+            case "×": performOperation { $0 * $1}
+            case "÷": performOperation { $1 / $0}
+            case "+": performOperation { $0 + $1}
+            case "−": performOperation { $1 - $0}
+            case "√": performOperation2 { sqrt($0)}
+            default : break
+        }
+    }
+    
+    /**
+        Overloading issue https://www.reddit.com/r/swift/comments/34yusl/help_with_function_overloading/
+    
+    
+    */
+    func performOperation(operation: (Double, Double) -> Double) {
+    
+            if operandStack.count >= 2 {
+                displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+                enter()
+            }
+            
+    }
+    
+    func performOperation2(operation: (Double) -> Double) {
+        
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+        
+    }
+
+    
+    //Bad form in swift can be infered the type
+    //var operandStack: Array<Double> = Array<Double>()
+    var operandStack = Array<Double>()
+
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+        
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+            
         }
     }
 
